00c2ea52301f045ef8105f8c1702730c764fc05c
PK-1304 <pushkark1304@gmail.com>
2026-05-28 20:56:48 +0530

    websocket config file

---
+++ client/src/lib/socket.js
@@ -0,0 +1,17 @@
+import { io } from "socket.io-client";
+
+let socket = null;
+
+export function connectSocket() {
+  if (socket?.connected) return socket;
+
+  socket = io("http://localhost:3000");
+  return socket;
+}
+
+export function disconnectSocket() {
+  if (!socket) throw new Error("No socket instance found");
+
+  socket.disconnect();
+  socket = null;
+}
