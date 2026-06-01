7df80d6c1a160e60f3b045bf886c654149dad787
PK-1304 <pushkark1304@gmail.com>
2026-06-01 16:59:29 +0530

    Prime react provider and context wrapped around main content

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,17 @@ import router from './router'
 import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <PrimeReactProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </PrimeReactProvider>
   </StrictMode>,
 )
