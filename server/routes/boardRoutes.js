b9567ad72943524558a8fbc47d4488a21072046a
PK-1304 <pushkark1304@gmail.com>
2026-06-11 12:53:01 +0530

    add route for api requests related to whiteboard

---
+++ server/routes/boardRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+
+const supabaseAdmin = require("../config/supabaseClient")
+
+// Import middlewares
+const boardAccess = require("../middlewares/boardAccess")
+
+router.get("/access/:boardId", boardAccess, async (req, res) => {
+    try {
+
+        const boardId = req.params.boardId
+        const { data: boardData, error } = await supabaseAdmin
+            .from('whiteboards')
+            .select('title, canvas_data')
+            .eq('id', boardId)
+            .single();
+
+        if (error) throw new Error(error)
+
+        return res.status(200).json({boardData, userRole: req.userRole})
+    }
+    catch(err){
+        console.log(err)
+        return res.status(400).json({"message": err})
+    }
+})
+
+module.exports = router
