a97f7991c789eb97d67dcc61423667134c2879b6
PK-1304 <pushkark1304@gmail.com>
2026-08-08 23:35:26 +0530

    edit the function to get user color from redis

--- server/utils/getUserData.js
+++ server/utils/getUserData.js
@@ -1,6 +1,11 @@
-const getUserData = (roomData, boardId, userId) => {
-    const user = roomData[boardId]?.find(user => user.userId === userId)
-    return {userColor: user?.userColor}
+const redis = require("../config/redisClient")
+
+const getUserData = async (boardId, userId) => {
+    // const user = roomData[boardId]?.find(user => user.userId === userId)
+    // return {userColor: user?.userColor}
+    const result = await redis.hget(`room:${boardId}`, userId)
+    const {userColor} = JSON.parse(result)
+    return userColor
 }
 
 module.exports = getUserData
